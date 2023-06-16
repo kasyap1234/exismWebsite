a89ade323795f4c1eb79bc92ac6de8fb2ed42b72
kasyap1234 <kasyap3103@gmail.com>
2023-06-17 01:56:12 +0530

    getting simulation errors

--- client/src/components/Amplitude.js
+++ client/src/components/Amplitude.js
@@ -4,14 +4,26 @@ import axios from 'axios';
 import { FormContext } from './FormContext';
 import { useNavigate } from 'react-router-dom';
 
-
 function Amplitude() {
   const { storeAmplitudeData } = useContext(FormContext);
   const [siteList, setSiteList] = useState(["", "", "", "", ""]);
   const [ampsiteList, setAmpSiteList] = useState(["", "", "", "", ""]);
   const [crustList, setCrustList] = useState(["", "", "", "", "","","",""]);
   const [ampCrustList, setAmpCrustList] = useState(["", "", "", "", "","","",""]);
-  const { sourceFormData, faultFormData, pathFormData } = useContext(FormContext);
+  const [latitude, setLatitude] = useState("");
+const [longitude, setLongitude] = useState("");
+
+const handleLatitudeChange = (event) => {
+    setLatitude(event.target.value);
+};
+
+const handleLongitudeChange = (event) => {
+    setLongitude(event.target.value);
+};
+  const { sourceFormData,  pathFormData } = useContext(FormContext);
+
+ 
+
   const handleAmpChange = (index, event) => {
     let newList = [...ampsiteList];
     newList[index] = event.target.value;
@@ -39,33 +51,52 @@ function Amplitude() {
   const saveData = () => {
     const ampData={ siteList, ampsiteList, crustList, ampCrustList }
     storeAmplitudeData(ampData);
-     console.log(ampData); 
-
+    console.log(ampData); 
   };
+  const filenameSite = `site-${siteList.join("-")}-${ampsiteList.join("-")}`;
+const filenameCrust = `crust-${crustList.join("-")}-${ampCrustList.join("-")}`;
+
   
-    
-  const handleSendData = async () => {
-      const data = {
-          source: sourceFormData,
-          fault: faultFormData,
-          path: pathFormData,
-         
-      };
-      
-      try {
-          const response = await axios.post('http://localhost:8000/createSimulation', data);
+const handleSendData = async () => {
+  if (!latitude || !longitude) {
+      alert("Please provide both latitude and longitude");
+      return;
+  }
 
-          console.log(response.data);
-      } catch (error) {
-          console.error('Error:', error);
-      }
+  const siteAmp = {
+      freq: siteList,
+      amp: ampsiteList,
+      filename: filenameSite,
+      header: "Site Amplitude",
   };
+
+  const crustalAmp = {
+      freq: crustList,
+      amp: ampCrustList,
+      filename: filenameCrust,
+      header: "Crust Amplitude",
+  };
+
+  const data = {
+      path: pathFormData,
+      source: sourceFormData,
+      amplification: { site_amp: siteAmp, crustal_amp: crustalAmp },
+      site: {coordinates: [parseFloat(latitude), parseFloat(longitude)], site_coord_flag: 2},
+  };
+
+  try {
+      const response = await axios.post('http://localhost:8000/createSimulation', data);
+      console.log(response.data);
+  } catch (error) {
+      console.error('Error:', error);
+  }
+};
 
 
   return (
-    <div>
- <Box m={4}>
+    <Box m={4}>
       <Grid container spacing={3} direction="column">
+        {/* Site Amplitude Form */}
         <Grid item>
           <h2>Site Amplitude</h2>
           <Grid container spacing={2}>
@@ -95,6 +126,7 @@ function Amplitude() {
           </Grid>
         </Grid>
 
+        {/* Crust Amplitude Form */}
         <Grid item>
           <h2>Crust Amplitude</h2>
           <p>Frequency</p>
@@ -122,17 +154,38 @@ function Amplitude() {
                 />
               </Grid>
             ))}
+
+            {/* Site Coordinates */}
+            <Grid item>
+            <TextField
+    label="Latitude"
+    variant="outlined"
+    value={latitude}
+    onChange={handleLatitudeChange}
+/>
+<TextField
+    label="Longitude"
+    variant="outlined"
+    value={longitude}
+    onChange={handleLongitudeChange}
+/>
+            </Grid>
           </Grid>
         </Grid>
 
+        {/* Buttons */}
         <Grid item>
-          <Button variant="contained" onClick={handleSendData}>Send Data</Button>
+          <Grid container justifyContent="space-between" alignItems="center" spacing={2}>
+            <Grid item>
+              <Button variant="contained" onClick={saveData}> Save Data </Button>
+            </Grid>
+            <Grid item>
+              <Button variant="contained" onClick={handleSendData}>Send Data</Button>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </Box>
-       
-
-    </div>
   );
 }
 
